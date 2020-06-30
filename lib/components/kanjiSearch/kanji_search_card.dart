ac346eb25c885676bb0c69a7e17961c033dba251
h7x4abk3g <h7x4abk3g@protonmail.com>
2020-06-30 15:15:31 +0200

    Update

--- lib/components/kanjiSearch/kanji_search_card.dart
+++ lib/components/kanjiSearch/kanji_search_card.dart
@@ -1,19 +1,22 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
+import 'package:unofficial_jisho_api/api.dart';
+
 class KanjiResultCard extends StatelessWidget {
+
+  KanjiResult _result;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100,
       width: 100,
-      child: Text('Test'),
+      child: Text(_result.query),
     );
   }
 
-  KanjiResultCard(var result) {
-    
+  KanjiResultCard(KanjiResult result) {
+    this._result = result;
   }
+
 }
