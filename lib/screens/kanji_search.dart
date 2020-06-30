f0d8aabe0fbb22bbccb2ebede13c79ad7fbb8b72
h7x4abk3g <h7x4abk3g@protonmail.com>
2020-06-30 16:27:19 +0200

    Update

--- lib/screens/kanji_search.dart
+++ lib/screens/kanji_search.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:jisho_study_tool/components/kanjiSearch/kanji_search_card.dart';
 import 'package:jisho_study_tool/services/jisho_search.dart';
 
-class KanjiSearch extends StatefulWidget {
+class KanjiSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [searchKanji('金')],
-    );
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    return searchForKanji('谷');
   }
 
 }
--- lib/services/jisho_search.dart
+++ lib/services/jisho_search.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:unofficial_jisho_api/api.dart';
+import 'package:unofficial_jisho_api/api.dart' as jisho;
 
 import 'package:jisho_study_tool/components/kanjiSearch/kanji_search_card.dart';
 
-Future<Widget> searchKanji(String kanji) async {
-  KanjiResult result = await searchForKanji(kanji);
-  return KanjiResultCard(result);
+Widget searchForKanji(String kanji) {
+  return FutureBuilder(
+    future: jisho.searchForKanji(kanji),
+    builder: (BuildContext context, AsyncSnapshot<jisho.KanjiResult> snapshot) {
+      return KanjiResultCard(snapshot.data);
+    }
+  );
 }
