0a9dba4d2e6e463fec34d4bc15a8fe2bbcbbde26
h7x4abk3g <h7x4abk3g@protonmail.com>
2020-07-13 21:38:24 +0200

    Move searchbar

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jisho_study_tool/components/search_bar.dart';
 
 class SearchView extends StatelessWidget {
   @override
@@ -9,6 +8,62 @@ class SearchView extends StatelessWidget {
       children: [
         SearchBar(),
       ],
+    );
+  }
+}
+
+class _LanguageOption extends StatelessWidget {
+  final String _language;
+  final Color _color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: Center(child: Text(_language)),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 1.0,
+          ),
+          color: _color
+        ),
+      ),
+    );
+  }
+
+  _LanguageOption(this._language, this._color);
+}
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: TextEditingController(),
+            decoration: InputDecoration(
+              labelText: 'Search',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            children: [
+              _LanguageOption('Auto', Colors.white), 
+              _LanguageOption('English', Colors.white),
+              _LanguageOption('Japanese', Colors.blue),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
