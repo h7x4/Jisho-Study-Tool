4bfa16074a93a3c23e5d8113f2c960a59dd50284
h7x4abk3g <h7x4abk3g@protonmail.com>
2020-07-21 00:00:38 +0200

    Finish layout

--- lib/components/kanji/kanji_suggestions.dart
+++ lib/components/kanji/kanji_suggestions.dart
@@ -7,7 +7,42 @@ class KanjiSuggestions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: Colors.grey[300],
+      padding: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 40.0,
+      ),
+      child: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 20.0,
+        crossAxisSpacing: 40.0,
+        
+        children: _suggestions.map((kanji) => _Suggestion(kanji)).toList(),
+      ),
+    );
+  }
+}
+
+class _Suggestion extends StatelessWidget {
+  final String _kanji;
+  const _Suggestion(this._kanji);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: FittedBox(
+          child: Text(
+            _kanji,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
