2facc4136aae7b57282163703398dcceb9d43469
h7x4abk3g <h7x4abk3g@protonmail.com>
2020-07-22 13:17:21 +0200

    Add custom theme class

---
+++ lib/themes/custom_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  final Color primaryColor;
+  final Color primaryText;
+  final Color kunyomiColor;
+  final Color kunyomiText;
+  final Color onyomiColor;
+  final Color onyomiText;
+  final Color meaningColor;
+  final Color meaningText;
+  final Color errorColor;
+  final Color backgroundColor;
+
+  const CustomTheme({
+    this.primaryColor,
+    this.primaryText,
+    this.kunyomiColor,
+    this.kunyomiText,
+    this.onyomiColor,
+    this.onyomiText,
+    this.meaningColor,
+    this.meaningText,
+    this.errorColor,
+    this.backgroundColor,
+  });
+}
---
+++ lib/themes/dark.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  primaryColor: Color(0x56D926),
+);
