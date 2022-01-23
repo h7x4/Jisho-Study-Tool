e994d73506b5fcb4c38e1aecabb9bdcbfa088665
h7x4 <h7x4abk3g@protonmail.com>
2022-01-23 16:25:43 +0100

    Make kanji widgets squared

--- lib/components/search/search_results_body/parts/kanji.dart
+++ lib/components/search/search_results_body/parts/kanji.dart
@@ -5,10 +5,42 @@ import '../../../../routing/routes.dart';
 
 class KanjiRow extends StatelessWidget {
   final List<String> kanji;
+  final double fontSize;
   const KanjiRow({
     Key? key,
     required this.kanji,
+    this.fontSize = 20,
   }) : super(key: key);
+
+  Widget _kanjiBox(String kanji) => UnconstrainedBox(
+        child: IntrinsicHeight(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: BlocBuilder<ThemeBloc, ThemeState>(
+              builder: (context, state) {
+                final colors = state.theme.menuGreyLight;
+                return Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: colors.background,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: FittedBox(
+                    child: Text(
+                      kanji,
+                      style: TextStyle(
+                        color: colors.foreground,
+                        fontSize: fontSize,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +48,7 @@ class KanjiRow extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text(
-          'Kanji',
+          'Kanji:',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 5),
@@ -26,21 +58,12 @@ class KanjiRow extends StatelessWidget {
           children: kanji
               .map(
                 (k) => InkWell(
-                  onTap: () => Navigator.pushNamed(context, Routes.kanjiSearch, arguments: k),
-                  child: BlocBuilder<ThemeBloc, ThemeState>(
-                    builder: (context, state) {
-                      final colors = state.theme.menuGreyLight;
-                      return Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                        color: colors.background,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child:
-                            Text(k, style: TextStyle(color: colors.foreground, fontSize: 25)),
-                      );
-                    },
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    Routes.kanjiSearch,
+                    arguments: k,
                   ),
+                  child: _kanjiBox(k),
                 ),
               )
               .toList(),
