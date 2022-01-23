3d30a81205f15d1360d4739d28e60b2f6353144f
h7x4 <h7x4ABk3g@protonmail.com>
2022-01-23 21:00:45 +0100

    Add search buttons and explanations (#32)
    * Add search button
    
    * Add search tip to kanji search

--- lib/components/search/search_bar.dart
+++ lib/components/search/search_bar.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../models/themes/theme.dart';
 import '../../routing/routes.dart';
 import 'language_selector.dart';
 
 class SearchBar extends StatelessWidget {
-  const SearchBar({Key? key}) : super(key: key);
+  final TextEditingController controller = TextEditingController();
+
+  SearchBar({Key? key}) : super(key: key);
+
+  void _search(BuildContext context, String text) => Navigator.pushNamed(
+        context,
+        Routes.search,
+        arguments: text,
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -13,17 +22,34 @@ class SearchBar extends StatelessWidget {
       child: Column(
         children: [
           TextField(
-            onSubmitted: (text) =>
-                Navigator.pushNamed(context, Routes.search, arguments: text),
-            controller: TextEditingController(),
+            onSubmitted: (text) => _search(context, text),
+            controller: controller,
             decoration: InputDecoration(
               labelText: 'Search',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
+              suffixIconConstraints: const BoxConstraints.tightFor(height: 60),
+              suffixIcon: Material(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+                color: AppTheme.jishoGreen.background,
+                child: IconButton(
+                  onPressed: () {
+                    if (controller.text.isNotEmpty)
+                      _search(context, controller.text);
+                  },
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ),
-          const SizedBox(height: 10.0),
+          const SizedBox(height: 20),
           const LanguageSelector()
         ],
       ),
